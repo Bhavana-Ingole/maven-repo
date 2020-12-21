54d9b3193b6cd0fd782e0c89a248b50c76bdd77f
Bhavana Ingole <Bhavanaingole456@gmail.com>
2020-12-21 15:16:33 +0530

    div method added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -16,10 +16,16 @@ public class Calc
 		return n1*n2;
 	}
 	
+	public static int div(int n1,int n2)
+	{
+		return n1/n2;
+	}
+	
 	public static void main(String args[])
 	{
 		System.out.println("Addition is : "+add(10,20));
 		System.out.println("Subtraction is : "+sub(40,20));
 		System.out.println("multipliction is : "+mul(40,20));
+		System.out.println("Division is : "+div(40,20));
 	}
 }
